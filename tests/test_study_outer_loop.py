644a9077309b03122d00011d52c41e9956713778
Feng GAO <gaofeng21cn@hotmail.com>
2026-05-20 15:47:59 +0800

    test: include publication gate recheck lifecycle cases

--- tests/test_study_outer_loop.py
+++ tests/test_study_outer_loop.py
@@ -16,6 +16,7 @@ from .test_study_outer_loop_cases.fast_lane_closeout_cases import *  # noqa: F40
 from .test_study_outer_loop_cases.runtime_resume_cases import *  # noqa: F403,F401
 from .test_study_outer_loop_cases.owner_priority_cases import *  # noqa: F403,F401
 from .test_study_outer_loop_cases.publication_gate_cases import *  # noqa: F403,F401
+from .test_study_outer_loop_cases.publication_gate_recheck_lifecycle_cases import *  # noqa: F403,F401
 from .test_study_outer_loop_cases.quality_repair_priority_cases import *  # noqa: F403,F401
 from .test_study_outer_loop_cases.submission_milestone_parking_regression_cases import *  # noqa: F403,F401
 from .test_study_outer_loop_cases.stopped_submission_milestone_cases import *  # noqa: F403,F401
