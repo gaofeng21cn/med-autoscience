b31330c0254e58eb3b1a36a4b781c73885c740fa
Feng GAO <gaofeng21cn@hotmail.com>
2026-05-22 05:15:22 +0800

    refactor: split study workbench conversation projection

--- src/med_autoscience/controllers/progress_portal_parts/study_workbench_parts/__init__.py
+++ src/med_autoscience/controllers/progress_portal_parts/study_workbench_parts/__init__.py
@@ -5,9 +5,15 @@ from .action_routing import (
     render_action_owner_routing_section,
     workbench_summary,
 )
+from .conversation import (
+    build_conversation_projection,
+    render_conversation_section,
+)
 
 __all__ = [
     "action_owner_routing_policy",
+    "build_conversation_projection",
     "render_action_owner_routing_section",
+    "render_conversation_section",
     "workbench_summary",
 ]
