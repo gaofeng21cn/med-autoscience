f747cb734331ac15446a8bc584071f198f225919
Feng GAO <gaofeng21cn@hotmail.com>
2026-06-14 08:23:43 +0800

    cli: restore study progress markdown export

--- src/med_autoscience/controllers/study_progress.py
+++ src/med_autoscience/controllers/study_progress.py
@@ -6,3 +6,4 @@ from .study_progress_parts.progression import *
 from .study_progress_parts.operator_view import *
 from .study_progress_parts.projection import *
 from .study_progress_parts.user_visible_projection import *
+from .study_progress_parts.markdown_projection import *
