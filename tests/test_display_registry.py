941fbe6a7b8ae4dd654884c110313ed0917f5082
Feng GAO <gaofeng21cn@hotmail.com>
2026-06-14 08:25:15 +0800

    test(display): align registry renderer assertions

--- tests/test_display_registry.py
+++ tests/test_display_registry.py
@@ -128,7 +128,7 @@ def test_time_to_event_threshold_governance_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("A", "B")
     assert spec.evidence_class == "clinical_utility"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "time_to_event_threshold_governance_inputs_v1"
     assert spec.layout_qc_profile == "publication_time_to_event_threshold_governance_panel"
 
@@ -138,7 +138,7 @@ def test_genomic_program_governance_summary_panel_is_registered() -> None:
 
     assert spec.template_id == _full_id("genomic_program_governance_summary_panel")
     assert spec.paper_family_ids == ("G",)
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "genomic_program_governance_summary_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_genomic_program_governance_summary_panel"
 
@@ -158,7 +158,7 @@ def test_time_to_event_multihorizon_calibration_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("A", "B")
     assert spec.evidence_class == "time_to_event"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "time_to_event_multihorizon_calibration_inputs_v1"
     assert spec.layout_qc_profile == "publication_time_to_event_multihorizon_calibration_panel"
 
@@ -310,7 +310,7 @@ def test_pathway_enrichment_dotplot_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("E", "G")
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "pathway_enrichment_dotplot_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_pathway_enrichment_dotplot_panel"
 
@@ -320,7 +320,7 @@ def test_celltype_marker_dotplot_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("D", "E", "G")
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "celltype_marker_dotplot_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_celltype_marker_dotplot_panel"
 
@@ -340,7 +340,7 @@ def test_omics_volcano_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "data_geometry"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "omics_volcano_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_omics_volcano_panel"
 
@@ -350,7 +350,7 @@ def test_oncoplot_mutation_landscape_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "oncoplot_mutation_landscape_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_oncoplot_mutation_landscape_panel"
 
@@ -360,7 +360,7 @@ def test_cnv_recurrence_summary_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "cnv_recurrence_summary_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_cnv_recurrence_summary_panel"
 
@@ -370,7 +370,7 @@ def test_genomic_alteration_landscape_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "genomic_alteration_landscape_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_genomic_alteration_landscape_panel"
 
@@ -380,7 +380,7 @@ def test_genomic_alteration_consequence_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "genomic_alteration_consequence_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_genomic_alteration_consequence_panel"
 
@@ -390,7 +390,7 @@ def test_genomic_alteration_multiomic_consequence_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "genomic_alteration_multiomic_consequence_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_genomic_alteration_multiomic_consequence_panel"
 
@@ -400,7 +400,7 @@ def test_genomic_alteration_pathway_integrated_composite_panel_is_registered() -
 
     assert spec.paper_family_ids == ("G",)
     assert spec.evidence_class == "matrix_pattern"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "genomic_alteration_pathway_integrated_composite_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_genomic_alteration_pathway_integrated_composite_panel"
 
@@ -410,7 +410,7 @@ def test_shap_waterfall_local_explanation_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("F",)
     assert spec.evidence_class == "model_explanation"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "shap_waterfall_local_explanation_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_shap_waterfall_local_explanation_panel"
 
@@ -420,7 +420,7 @@ def test_shap_force_like_summary_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("F",)
     assert spec.evidence_class == "model_explanation"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "shap_force_like_summary_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_shap_force_like_summary_panel"
 
@@ -480,7 +480,7 @@ def test_shap_bar_importance_is_registered() -> None:
 
     assert spec.paper_family_ids == ("F",)
     assert spec.evidence_class == "model_explanation"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "shap_bar_importance_inputs_v1"
     assert spec.layout_qc_profile == "publication_shap_bar_importance"
 
@@ -500,7 +500,7 @@ def test_shap_multicohort_importance_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("F",)
     assert spec.evidence_class == "model_explanation"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "shap_multicohort_importance_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_shap_multicohort_importance_panel"
 
@@ -604,7 +604,7 @@ def test_generalizability_subgroup_composite_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("C", "H")
     assert spec.evidence_class == "generalizability"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "generalizability_subgroup_composite_inputs_v1"
     assert spec.layout_qc_profile == "publication_generalizability_subgroup_composite_panel"
 
@@ -614,7 +614,7 @@ def test_compact_effect_estimate_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("C", "H")
     assert spec.evidence_class == "effect_estimate"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "compact_effect_estimate_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_compact_effect_estimate_panel"
 
@@ -624,7 +624,7 @@ def test_coefficient_path_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("C", "H")
     assert spec.evidence_class == "effect_estimate"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "coefficient_path_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_coefficient_path_panel"
 
@@ -634,7 +634,7 @@ def test_broader_heterogeneity_summary_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("C", "H")
     assert spec.evidence_class == "effect_estimate"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "broader_heterogeneity_summary_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_broader_heterogeneity_summary_panel"
 
@@ -644,7 +644,7 @@ def test_interaction_effect_summary_panel_is_registered() -> None:
 
     assert spec.paper_family_ids == ("C", "H")
     assert spec.evidence_class == "effect_estimate"
-    assert spec.renderer_family == "python"
+    assert spec.renderer_family == "r_ggplot2"
     assert spec.input_schema_id == "interaction_effect_summary_panel_inputs_v1"
     assert spec.layout_qc_profile == "publication_interaction_effect_summary_panel"
 
@@ -784,18 +784,18 @@ def test_time_to_event_publication_surface_specs_are_registered() -> None:
     assert figure7.input_schema_id == "binary_prediction_curve_inputs_v1"
     assert figure7.template_id == _full_id("time_dependent_roc_horizon")
     assert figure7.evidence_class == "time_to_event"
-    assert figure8.renderer_family == "python"
+    assert figure8.renderer_family == "r_ggplot2"
     assert figure8.input_schema_id == "time_dependent_roc_comparison_inputs_v1"
     assert figure8.layout_qc_profile == "publication_evidence_curve"
     assert figure8b.paper_family_ids == ("A", "B")
     assert figure8b.evidence_class == "time_to_event"
-    assert figure8b.renderer_family == "python"
+    assert figure8b.renderer_family == "r_ggplot2"
     assert figure8b.input_schema_id == "time_to_event_landmark_performance_inputs_v1"
     assert figure8b.layout_qc_profile == "publication_landmark_performance_panel"
     assert figure9.layout_qc_profile == "publication_embedding_scatter"
     assert figure9b.paper_family_ids == ("D", "E", "G")
     assert figure9b.evidence_class == "data_geometry"
-    assert figure9b.renderer_family == "python"
+    assert figure9b.renderer_family == "r_ggplot2"
     assert figure9b.input_schema_id == "celltype_signature_heatmap_inputs_v1"
     assert figure9b.layout_qc_profile == "publication_celltype_signature_panel"
     assert figure10.input_schema_id == "performance_heatmap_inputs_v1"
@@ -815,31 +815,31 @@ def test_time_to_event_publication_surface_specs_are_registered() -> None:
     assert figure10c.layout_qc_profile == "publication_heatmap"
     assert figure10d.paper_family_ids == ("E", "G")
     assert figure10d.evidence_class == "matrix_pattern"
-    assert figure10d.renderer_family == "python"
+    assert figure10d.renderer_family == "r_ggplot2"
     assert figure10d.input_schema_id == "pathway_enrichment_dotplot_panel_inputs_v1"
     assert figure10d.layout_qc_profile == "publication_pathway_enrichment_dotplot_panel"
     assert figure10e.paper_family_ids == ("D", "E", "G")
     assert figure10e.evidence_class == "matrix_pattern"
-    assert figure10e.renderer_family == "python"
+    assert figure10e.renderer_family == "r_ggplot2"
     assert figure10e.input_schema_id == "celltype_marker_dotplot_panel_inputs_v1"
     assert figure10e.layout_qc_profile == "publication_celltype_marker_dotplot_panel"
     assert figure12.input_schema_id == "forest_effect_inputs_v1"
     assert figure12.layout_qc_profile == "publication_forest_plot"
     assert figure13.paper_family_ids == ("F",)
     assert figure13.evidence_class == "model_explanation"
-    assert figure13.renderer_family == "python"
+    assert figure13.renderer_family == "r_ggplot2"
     assert figure13.input_schema_id == "shap_dependence_panel_inputs_v1"
     assert figure13.layout_qc_profile == "publication_shap_dependence_panel"
     figure13b = display_registry.get_evidence_figure_spec(_full_id("shap_waterfall_local_explanation_panel"))
     assert figure13b.paper_family_ids == ("F",)
     assert figure13b.evidence_class == "model_explanation"
-    assert figure13b.renderer_family == "python"
+    assert figure13b.renderer_family == "r_ggplot2"
     assert figure13b.input_schema_id == "shap_waterfall_local_explanation_panel_inputs_v1"
     assert figure13b.layout_qc_profile == "publication_shap_waterfall_local_explanation_panel"
     figure13c = display_registry.get_evidence_figure_spec(_full_id("shap_force_like_summary_panel"))
     assert figure13c.paper_family_ids == ("F",)
     assert figure13c.evidence_class == "model_explanation"
-    assert figure13c.renderer_family == "python"
+    assert figure13c.renderer_family == "r_ggplot2"
     assert figure13c.input_schema_id == "shap_force_like_summary_panel_inputs_v1"
     assert figure13c.layout_qc_profile == "publication_shap_force_like_summary_panel"
     figure13d = display_registry.get_evidence_figure_spec(_full_id("partial_dependence_ice_panel"))
@@ -848,7 +848,7 @@ def test_time_to_event_publication_surface_specs_are_registered() -> None:
     assert figure13d.renderer_family == "python"
     assert figure13d.input_schema_id == "partial_dependence_ice_panel_inputs_v1"
     assert figure13d.layout_qc_profile == "publication_partial_dependence_ice_panel"
-    assert figure14.renderer_family == "python"
+    assert figure14.renderer_family == "r_ggplot2"
     assert figure14.required_exports == ("png", "pdf")
     assert figure14.input_schema_id == "time_to_event_discrimination_calibration_inputs_v1"
     assert figure15.input_schema_id == "time_to_event_grouped_inputs_v1"
@@ -857,18 +857,18 @@ def test_time_to_event_publication_surface_specs_are_registered() -> None:
     assert figure17.evidence_class == "generalizability"
     assert figure17b.paper_family_ids == ("C", "H")
     assert figure17b.evidence_class == "generalizability"
-    assert figure17b.renderer_family == "python"
+    assert figure17b.renderer_family == "r_ggplot2"
     assert figure17b.input_schema_id == "generalizability_subgroup_composite_inputs_v1"
     assert figure17b.layout_qc_profile == "publication_generalizability_subgroup_composite_panel"
-    assert figure19.renderer_family == "python"
+    assert figure19.renderer_family == "r_ggplot2"
     assert figure19.input_schema_id == "time_to_event_stratified_cumulative_incidence_inputs_v1"
     assert figure19.layout_qc_profile == "publication_survival_curve"
-    assert figure22.renderer_family == "python"
+    assert figure22.renderer_family == "r_ggplot2"
     assert figure22.input_schema_id == "risk_layering_monotonic_inputs_v1"
     assert figure22.layout_qc_profile == "publication_risk_layering_bars"
     assert figure3.input_schema_id == "binary_calibration_decision_curve_panel_inputs_v1"
     assert figure3.layout_qc_profile == "publication_binary_calibration_decision_curve"
-    assert figure3.renderer_family == "python"
+    assert figure3.renderer_family == "r_ggplot2"
     assert figure4.evidence_class == "model_audit"
     assert figure4.input_schema_id == "model_complexity_audit_panel_inputs_v1"
     assert figure4.layout_qc_profile == "publication_model_complexity_audit"
