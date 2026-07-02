05f4fc9b1eab68cdd60d18e3c2ae2d4ebfc00b6f
Feng GAO <gaofeng21cn@hotmail.com>
2026-07-02 08:03:25 +0800

    Add reference authenticity attestation kernel

---
+++ src/med_autoscience/research_integrity/__init__.py
@@ -0,0 +1,19 @@
+from med_autoscience.research_integrity.reference_authenticity import (
+    SCHEMA_VERSION,
+    SURFACE_KIND,
+    SUPPORTED_PROVIDERS,
+    ReferenceVerificationAttestation,
+    ReferenceVerificationStatus,
+    build_reference_verification_attestation,
+    build_reference_verification_attestation_dict,
+)
+
+__all__ = [
+    "SCHEMA_VERSION",
+    "SURFACE_KIND",
+    "SUPPORTED_PROVIDERS",
+    "ReferenceVerificationAttestation",
+    "ReferenceVerificationStatus",
+    "build_reference_verification_attestation",
+    "build_reference_verification_attestation_dict",
+]
