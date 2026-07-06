7adf7b2190295e0de7f4c8d87478518b35b5343c
Feng GAO <gaofeng21cn@hotmail.com>
2026-07-07 04:26:23 +0800

    Thin medical publication surface facade exports

--- src/med_autoscience/controllers/medical_publication_surface.py
+++ src/med_autoscience/controllers/medical_publication_surface.py
@@ -1,7 +1,30 @@
 from __future__ import annotations
 
-from .medical_publication_surface_parts.asset_scans import *
-from .medical_publication_surface_parts.catalog_checks import *
-from .medical_publication_surface_parts.manuscript_checks import *
-from .medical_publication_surface_parts.reporting import *
-from .medical_publication_surface_parts.shared import *
+from importlib import import_module
+from types import ModuleType
+
+_PART_MODULES = (
+    "asset_scans",
+    "catalog_checks",
+    "manuscript_checks",
+    "reporting",
+    "shared",
+)
+
+
+def _reexport(module: ModuleType) -> None:
+    names = getattr(module, "__all__", None)
+    if names is None:
+        names = tuple(name for name in vars(module) if not name.startswith("_"))
+    for name in names:
+        globals()[name] = getattr(module, name)
+
+
+for _module_name in _PART_MODULES:
+    _reexport(import_module(f"{__package__}.medical_publication_surface_parts.{_module_name}"))
+
+del import_module, ModuleType
+
+__all__ = tuple(name for name in globals() if not name.startswith("_"))
+
+del _PART_MODULES, _module_name, _reexport
