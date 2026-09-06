eb35f415e3fc2573e94066531b92393ca6368b53
Feng GAO <gaofeng21cn@hotmail.com>
2026-09-06 11:14:01 +0800

    fix: register study initialization in source conformance

--- scripts/repo_hygiene_audit.py
+++ scripts/repo_hygiene_audit.py
@@ -75,6 +75,7 @@ EXPECTED_STANDARD_AGENT_SOURCE_FILES = frozenset(
         "src/med_autoscience/authority_handlers/paper_mission.py",
         "src/med_autoscience/authority_handlers/qualification_work_item_provisioning.py",
         "src/med_autoscience/authority_handlers/self_evolution_closeout.py",
+        "src/med_autoscience/authority_handlers/study_initialization.py",
         "src/med_autoscience/authority_handlers/study_lifecycle_reactivation.py",
         "src/med_autoscience/authority_handlers/_stage_attempt_review_snapshot.py",
         "src/med_autoscience/styles/__init__.py",
